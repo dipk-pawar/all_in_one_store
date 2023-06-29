7f8c89a928d04374e10c439b9af4db2d37965e42
Dipak Pawar <pawardipak482@gmail.com>
2023-06-29 22:13:52 +0530

    added the prdocts

--- apps/store/views.py
+++ apps/store/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Product
+
 
 # Create your views here.
 def home(request):
-	return render(request, "home.html")
+    products = Product.objects.filter(is_available=True)
+    return render(request, "home.html", context={"products": products})
