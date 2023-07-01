b76f21c82ed7fef6bbadef5d484876ff72f8d3d4
Dipak Pawar <pawardipak482@gmail.com>
2023-07-01 14:41:44 +0530

    added the filter for categories

--- apps/store/urls.py
+++ apps/store/urls.py
@@ -1,4 +1,7 @@
 from apps.store.views import store
 from django.urls import path
 
-urlpatterns = [path("", store, name="store")]
+urlpatterns = [
+    path("", store, name="store"),
+    path("<slug:category_slug>", store, name="product_with_category"),
+]
--- apps/store/views.py
+++ apps/store/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Product
+from apps.category.models import Category
 
 
 # Create your views here.
-def store(request):
-    products = Product.objects.filter(is_available=True)
+def store(request, category_slug=None):
+    if category_slug is not None:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=category, is_available=True)
+    else:
+        products = Product.objects.all()
     product_counts = products.count()
     return render(
         request,
