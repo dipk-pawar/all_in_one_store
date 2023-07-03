5577bfb820c23fd392a9b5dc378cd5829f6703e2
Dipak Pawar <pawardipak482@gmail.com>
2023-07-03 23:15:22 +0530

    added the add to cart functionality

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,6 +1,7 @@
-from .views import cart
+from .views import cart, add_cart
 from django.urls import path
 
 urlpatterns = [
     path("", cart, name="cart"),
+    path("add_cart/<int:product_id>/", add_cart, name="add_cart"),
 ]
--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from apps.store.models import Product
+from .models import Cart, CartItem
 
 
 # Create your views here.
 def cart(request):
     return render(request, "store/cart.html")
+
+
+def _cart_id(requst):
+    return requst.session.session_key or requst.session.create()
+
+
+def add_cart(requst, product_id):
+    product = Product.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(requst))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(cart_id=_cart_id(requst))
+        cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(product=product, cart=cart, quantity=1)
+        cart_item.save()
+
+    return redirect("cart")
