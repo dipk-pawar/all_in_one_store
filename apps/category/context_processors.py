5efad1d8d03c99eedb664654d058cd82d85a23f2
Dipak Pawar <pawardipak482@gmail.com>
2023-08-13 21:45:37 +0530

    added count in cart

--- apps/category/context_processors.py
+++ apps/category/context_processors.py
@@ -1,6 +1,13 @@
+from apps.cart.views import _cart_id
 from .models import Category
+from apps.cart.models import Cart, CartItem
 
 
 def menu_links(request):
     links = Category.objects.all()
-    return dict(links=links)
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_item_count = CartItem.objects.filter(cart=cart).count()
+    except Cart.DoesNotExist:
+        cart_item_count = 0
+    return dict(links=links, cart_item_count=cart_item_count)
